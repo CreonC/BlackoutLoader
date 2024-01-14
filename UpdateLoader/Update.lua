ea7740879677b5b4cb545a95d0298f1c5ad0e198
CreonC <69414582+CreonC@users.noreply.github.com>
2024-01-14 21:15:58 +0800

    Fix space

--- UpdateLoader/Update.lua
+++ UpdateLoader/Update.lua
@@ -1,24 +1,17 @@
 print("Starting update...")
 local HttpService = game:GetService("HttpService")
-
-
 local UpstreamCommits = HttpService:JSONDecode(HttpService:GetAsync("https://api.github.com/repos/CreonC/BlackoutLoader/commits",false))
-
 local commitToClone = UpstreamCommits[1]
-
 local Lastestcommits = HttpService:JSONDecode(HttpService:GetAsync("https://api.github.com/repos/CreonC/BlackoutLoader/commits/"..tostring(commitToClone.sha),false))
 
 print(string.format("Got lastest commit sha: %s",string.sub(tostring(Lastestcommits.sha), 1, 7)))
 
 local ClientLoaderFile = HttpService:JSONDecode(HttpService:GetAsync("https://api.github.com/repos/CreonC/BlackoutLoader/contents/Client.lua",true))
-
 local ClientLoaderDownloadLink = ClientLoaderFile.download_url
 
 print("Got client download url, download client now...")
 
 local ClientLoaderFile2 = HttpService:GetAsync(ClientLoaderDownloadLink,true)
-
-
 local OldClientLoader = game.ReplicatedFirst.ProjectLoader
 
 if OldClientLoader.ClientGithash.Value == string.sub(tostring(Lastestcommits.sha), 1, 7) then
@@ -40,11 +33,8 @@ end
 print("Getting Server download url...")
 
 local ServerLoaderFile = HttpService:JSONDecode(HttpService:GetAsync("https://api.github.com/repos/CreonC/BlackoutLoader/contents/Server.lua",true))
-
 local ServerLoaderDownloadLink = ServerLoaderFile.download_url
-
 local ServerLoaderFile2 = HttpService:GetAsync(ServerLoaderDownloadLink,true)
-
 local OldServerLoader = game.ServerScriptService.ProjectLoader_Server
 
 if OldServerLoader.ServergitHash.Value == string.sub(tostring(Lastestcommits.sha), 1, 7) then
